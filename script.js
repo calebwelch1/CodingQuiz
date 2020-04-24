c78b40725bcbfba88673a91461ed0d4f7e90de26
calebwelch <caleb.welch11@gmail.com>
2020-04-24 15:16:07 -0400

    created buttons. Centered content. Created timer.

--- script.js
+++ script.js
@@ -23,9 +23,33 @@
 
 var startBtn = $("#startButton")
 
-setInterval(function () {
-    var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);//conversion miliseconds on seconds
-    if (seconds < 10) seconds = "0" + seconds;
-}, milliseconds)
+// setInterval(function () {
+//     var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);//conversion miliseconds on seconds
+//     if (seconds < 10) seconds = "0" + seconds;
+// }, milliseconds)
+var startTimer = 5;
+var time = startTimer * 60;
+var countDownEl = document.querySelector("#timer")
 
+function countdown() {
+    var minutes = Math.floor(time / 60);
+    var seconds = time % 60;
+    // template literal only works with a ` backtick! not single quote
+    // template literal syntax ${updating Var or Func}
+    countDownEl.innerHTML = `${minutes}:${seconds}`
+    time--;
+    if (minutes === 0 && seconds === 0) {
+        countDownEl.innerHTML = "Game Over!"
+    };
+}
+//setInterval calls a function every x milliseconds (function, milliseconds)
+setInterval(countdown, 1000);
 
+// window.onload = function () {
+//     if (window.jQuery) {
+//         // jQuery is loaded  
+//         alert("Yeah!");
+//     } else {
+//         // jQuery is not loaded
+//         alert("Doesn't Work");
+//     }
