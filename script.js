7b2eecfdbfaa5923f5d4bdf5e1600e35f8b88415
calebwelch <caleb.welch11@gmail.com>
2020-04-26 16:34:52 -0400

    working on getting answer buttons to register and change on click

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ var startBtn = $("#startButton")
 //     var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);//conversion miliseconds on seconds
 //     if (seconds < 10) seconds = "0" + seconds;
 // }, milliseconds)
-var startTimer = 5;
+var startTimer = 3;
 var time = startTimer * 60;
 var countDownEl = document.querySelector("#timer")
 
@@ -56,39 +56,86 @@ var answer4 = $("#answer4");
 var questionContainer = $(".questions");
 var answerButtons = $(".answer-btn");
 var questionEl = $("#question")
-var questionIndex = 0;
+let questionIndex = undefined;
+let shuffle = undefined;
 
 nextButton.addClass("hide")
 answerButtons.addClass("hide")
 startButton.on("click", game)
+$("#questionContainer").addClass("hide")
 
 function game() {
     // alert("started!")
     startButton.addClass("hide");
     nextButton.removeClass("hide");
     answerButtons.removeClass("hide");
+    $("#timer").removeClass("hide");
+    questionEl.removeClass("hide")
+    $("#questionContainer").removeClass("hide")
+
+    shuffle = questions.sort(() => Math.random() - .5)
+    questionIndex = 0;
+
     nextQuestions()
 };
 function nextQuestions() {
-    chooseQuestion(questions[questionIndex]);
+    clear()
+    chooseQuestion(shuffle[questionIndex]);
 
 };
-function chooseQuestion(question) {
-    questionEl.innerText = questions.question
+function clear() {
+    nextButton.addClass("hide")
+    while (document.querySelector(".answerButtonsEl").firstChild) {
+        document.querySelector(".answerButtonsEl").removeChild
+            (document.querySelector(".answerButtonsEl").firstChild)
+    }
+}
+function chooseQuestion(thisquestion) {
+    // for some reason $("#please") won't work. Spent an hour figuring out to just use querySelector
+    document.querySelector("#please").innerHTML = thisquestion.question
+    thisquestion.answers.forEach(answer => {
+        let button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", correctAnswer)
+        document.querySelector(".answerButtonsEl").appendChild(button)
+    })
 };
-function answer() {
 
+function correctAnswer(ans) {
+    let selectedButton = ans.target
+    let correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    // convert to Array so we can use a for each loop on it. 
+    Array.from(document.querySelector(".answerButtonsEl").children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    nextButton.removeClass("hide")
 };
-
+function setStatusClass(ans, correct) {
+    clearStatus(ans)
+    if (correct) {
+        ans.addClass("correct")
+    } else {
+        ans.addClass("wrong")
+    }
+}
+function clearStatus(ans) {
+    ans.removeClass("correct")
+    ans.removeClass("wrong")
+}
 var questions = [
     {
-        question: "what is 1",
+        question: "what is 1?",
         answers: [
             { text: '4', correct: false },
             { text: '1', correct: true }
         ]
     }
-]
+];
 // window.onload = function () {
 //     if (window.jQuery) {
 //         // jQuery is loaded  
